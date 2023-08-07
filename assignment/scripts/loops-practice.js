6513f98fa2bf4516303bc57c8ecf70e48056158d
Said Said <ssaid20@icloud.com>
2023-08-06 22:01:29 -0500

    while loops

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -52,9 +52,22 @@ console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
-
+let i = 0;
+while (i < stars.length) {
+  console.log(stars[i]);
+  i++;
+}
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
-
+i = 0;
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+i = 10;
+while (i >= 5) {
+  console.log(i);
+  i--;
+}
